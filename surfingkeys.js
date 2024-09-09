7be7508924fece414fc1277eed3d4c6d2669509a
Hyunjun Kim <rmekdma@gmail.com>
2024-09-09 12:04:58 +0900

    Create surfingkeys.js

---
+++ surfingkeys.js
@@ -0,0 +1,37 @@
+settings.focusFirstCandidate = true;
+
+// set theme
+settings.theme = `
+.sk_theme {
+  font-family: SauceCodePro Nerd Font, Consolas, Menlo, monospace;
+  font-size: 10pt;
+  background: #f0edec;
+  color: #2c363c;
+}
+.sk_theme tbody {
+  color: #f0edec;
+}
+.sk_theme input {
+  color: #2c363c;
+}
+.sk_theme .url {
+  color: #1d5573;
+}
+.sk_theme .annotation {
+  color: #2c363c;
+}
+.sk_theme .omnibar_highlight {
+  color: #e4dfdd;
+}
+.sk_theme #sk_omnibarSearchResult {
+  background: #cdc4c1;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: #cdc4c1;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: #ceeece;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
